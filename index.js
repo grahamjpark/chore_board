7400eedb7f131078f9f60c28a08f7d3b637151c5
Graham Park <park580@purdue.edu>
2014-10-19 03:01:36 -0400

    target sockets, list updated per targets

--- index.js
+++ index.js
@@ -14,30 +14,30 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	console.log('user connection');
-	socket.on('register', function(clientID, user, pass, group){register(clientID, user, pass, group);});
-	socket.on('login', function(clientID, user, pass, group){login(clientID, user, pass, group);});
-	socket.on('addPublicJob', function(clientID, group, id, jobName, username, desc, value){addJob(clientID, true, group, id, jobName, username, desc, value);});
-	socket.on('addPrivateJob', function(clientID, group, id, jobName, username, desc, value){addJob(clientID, false, group, id, jobName, username, desc, value);});
-	socket.on('viewJobs', function(clientID, isPublic){viewJobs(clientID, isPublic);});
-	socket.on('viewLeaderboard', function(clientID){viewLeaderboard(clientID);});
-	socket.on('movePrivate', function(clientID, job, bounty, user){movePrivate(clientID, job, bounty, user);});
-	socket.on('submitBidJob', function(clientID, name, description, points){submitBidJob(clientID, name, description, points);});
-	socket.on('transferPoints', function(clientID, first, second, points){transferPoints(clientID, first, second, points);});
-	socket.on('bid', function(clientID, user, job, points){bid(clientID, user, job, points);});
-	socket.on('complete', function(clientID, job){complete(clientID, job);});
-	socket.on('verify', function(clientID, job, user){verify(clientID, job, user);});
+	socket.on('register', function(user, pass, group){register(socket, user, pass, group);});
+	socket.on('login', function(user, pass, group){login(socket, user, pass, group);});
+	socket.on('addPublicJob', function(group, id, jobName, username, desc, value){addJob(socket, true, group, id, jobName, username, desc, value);});
+	socket.on('addPrivateJob', function(group, id, jobName, username, desc, value){addJob(socket, false, group, id, jobName, username, desc, value);});
+	socket.on('viewJobs', function(isPublic){viewJobs(socket, isPublic);});
+	socket.on('viewLeaderboard', function(){viewLeaderboard(socket);});
+	socket.on('movePrivate', function(job, bounty, user){movePrivate(socket, job, bounty, user);});
+	socket.on('submitBidJob', function(name, description, points){submitBidJob(socket, name, description, points);});
+	socket.on('transferPoints', function(first, second, points){transferPoints(socket, first, second, points);});
+	socket.on('bid', function(user, job, points){bid(socket, user, job, points);});
+	socket.on('complete', function(job){complete(socket, job);});
+	socket.on('verify', function(job, user){verify(socket, job, user);});
 });
 
 //TODO: THESE FUNCTIONS
 //use the following to send things back to the index
 //io.emit('<functionname>', '<information>');
 
-function addJob(clientID, isPublic, group, id, jobName, username, desc, value) {
+function addJob(socket, isPublic, group, id, jobName, username, desc, value) {
 	var p = db.chores;
 	p.save({groupID: parseInt(group), ID: parseInt(id), name: jobName, user: username, description: desc, points: parseInt(value), isVerifying: false, isDone: false, isPublic: isPublic}, function(err, saved) {
 		if( err || !saved ) console.log("Job not saved");
 		else {
-			io.emit('addJob', clientID);
+			socket.emit('addJob');
 			console.log("Job saved");
 		}
 	});
@@ -57,7 +57,7 @@ function register(user, pass, group) {
 	});
 }
 
-function login(clientID, user, pass, group) {
+function login(socket, user, pass, group) {
 	//login/register [add user to db]
 	console.log('login ' + group + ': ' + user + ',' + pass);
 	db.users.find({username : user}, function(err, theUsers) {
@@ -68,7 +68,7 @@ function login(clientID, user, pass, group) {
 			console.log("No users found");
 			register(user, pass, group);
 			console.log("Registering " + group + ":" + user + "," + pass);
-			io.emit('login', clientID);
+			socket.emit('login');
 		} else theUsers.forEach( function(user) {
 			//login
 			//io.emit('addList', user.name + ': ' + user.description);
@@ -81,43 +81,43 @@ function login(clientID, user, pass, group) {
 	});
 }
 
-function viewJobs(clientID, isPublic) {
+function viewJobs(socket, isPublic) {
 	//view jobs (special | chores) [get jobs from db]
 	io.emit('clearList');
 	io.emit('setSection', isPublic ? 'Public' : 'Private');
 	db.chores.find({isPublic: isPublic}, function(err, chores) {
 		if( err || !chores) {
-			io.emit('addList', "No chores found");
+			socket.emit('addList', "No chores found");
 			console.log("No chores found");
 		} else chores.forEach( function(chore) {
-			io.emit('viewJobs', clientID);
-			io.emit('addList', chore.name + ': ' + chore.description);
+			socket.emit('viewJobs', chore.name, chore.description, chore.points, chore.isDone); //TODO:
+			//socket.emit('addList', chore.name + ': ' + chore.description);
 			console.log(chore.name + ': ' + chore.description);
 		});
 	});
 	console.log('view jobs public/private ' + isPublic);
 }
 
-function viewLeaderboard(clientID) {
+function viewLeaderboard(socket) {
 	//view leaderboard [get users from db]
-	io.emit('clearList');
-	io.emit('setSection', 'Leaderboard');
+	socket.emit('clearList');
+	socket.emit('setSection', 'Leaderboard');
 	db.users.find({}, function(err, users) {
 		if( err || !users) {
-			io.emit('addList', "No users found");
+			socket.emit('addList', "No users found");
 			console.log("No users found");
 		} else users.forEach( function(user) {
-			io.emit('addList', user.username + ': ' + user.points);
+			socket.emit('viewLeaderboard', user.username, user.points);
 			console.log(user.username + ': ' + user.points);
 		});
 	});
 	console.log('view leaderboard');
 }
 
-function movePrivate(clientID, job, bounty, user) {
+function movePrivate(socket, job, bounty, user) {
 	db.chores.find({ID: job, isPublic: false}, function(err, chores) {
 		if( err || !chores || chores.length == 0) {
-			io.emit('addList', "Job not found");
+			socket.emit('addList', "Job not found");
 			console.log("Job not found");
 		} else chores.forEach( function(chore) {
 			if (chore.user == user) {
@@ -143,42 +143,42 @@ function movePrivate(clientID, job, bounty, user) {
 				function(err, result) {console.log(err);console.log(result)});
 			}
 			console.log('move private ' + job + ',' + bounty);
-			io.emit('movePrivate', clientID);
+			socket.emit('movePrivate');
 		});
 	});
 }
 
-function submitBidJob(clientID, name, description, points) {
+function submitBidJob(socket, name, description, points) {
 	//submit special jobs (?) [add special job to db]
 	addJob(true, 0, currentIDIndex, name, "", description, points);
 	currentIDIndex++;
 	console.log('submit bid job ' + name + ',' + description + ',' + points);
 }
 
-function transferPoints(clientID, first, second, points) {
+function transferPoints(socket, first, second, points) {
 //sends points from first to second
 	db.users.update({username: first}, {$inc: {points: -points}});
 	db.users.update({username: second}, {$inc: {points: points}});
 	console.log('transfer ' + points + ' points from ' + first + ' to ' + second);
-	io.emit('transferPoints', clientID);
+	socket.emit('transferPoints');
 }
 
-function bid(clientID, user, job, points) {
+function bid(socket, user, job, points) {
 	db.chores.update({ID: job}, {$inc: {points: points}});
 	db.users.update({username: user}, {$inc: {points: -points}});
 	console.log(user + ' bid ' + points + ' points to ' + job);
-	io.emit('bid', clientID);
+	socket.emit('bid');
 }
 
-function complete(clientID, job, user) {
+function complete(socket, job, user) {
 	db.chores.update({ID: job}, {$set: {isDone: true}});
 	db.chores.update({ID: job}, {$set: {isVerifying: true}});
 	db.chores.update({ID: job}, {$set: {user: user}});
 	console.log(user + ' completed ' + job);
-	io.emit('complete', clientID);
+	socket.emit('complete');
 }
 
-function verify(clientID, job, user) {
+function verify(socket, job, user) {
 	//verify that a user did a special job [set job.isDone, job.isVerifying]
 	db.chores.update({ID: job}, {$set: {isDone: true}});
 	db.chores.update({ID: job}, {$set: {isVerifying: false}});
@@ -189,7 +189,7 @@ function verify(clientID, job, user) {
 			console.log("Job found");
 			console.log("points " + chore.points);
 			db.users.update({username: user}, {$inc: {points: chore.points}});
-			io.emit('verify', clientID);
+			socket.emit('verify');
 		});
 	});
 	console.log('verify ' + job + ',' + user);
